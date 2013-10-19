b6ae9ee38e9c6020e470e4f2e3638418087dc3ad
Greg Nikitin <greg@trackbike.com>
2013-10-19 13:32:49 -0400

    moved data to mongoLab and make all data calls in app.js

--- recipeSite/app.js
+++ recipeSite/app.js
@@ -6,39 +6,41 @@ var user = require('./routes/user'); //accesses routes/user
 var http = require('http'); //gets http framework
 var path = require('path'); //used to set paths
 //var mongo = require('mongodb'); 
-var monk = require('monk'); //imports monk (which is used to connect mongoDB)
-var db = monk('localhost:27017/recipecollection'); //use monk to link to db
+// var monk = require('monk'); //imports monk (which is used to connect mongoDB)
+// var db = monk('localhost:27017/recipecollection'); //use monk to link to db
+
+var collections = ['users'];
+// var db = require("mongojs").connect(dburl, collections);
+
+var mongojs = require('mongojs');
+
+// var db = mongojs('irvinetest:irvinetest@ds047438.mongolab.com:47438/styletest', ['Designer']);
+
+
 //var db2 = monk('localhost:27017/recipecollection'); //use monk to link to db
 // var mongojs = require('mongojs');
-// var db = mongojs('recipetest:recipetest@ds049288.mongolab.com:49288/recipes', ['Designer']);
+var db = mongojs('recipetest:recipetest@ds049288.mongolab.com:49288/recipes', ['Drinks']);
+// // app.js
 
-
-//CRAPPY BUT INTERESTING IDEA KEEP LOOKING
-// app.get('/search/', function(req,res){
-
-//   // Parse the query
-//   var dbQuery = url.parse(req.url, true).query;
-//   var product = dbQuery.product;
-//   var category = dbQuery.category;
-//   console.log('Searching for: ' + product + ' in ' + category);
-
-//   //Mongo DB setup then query
-//   var result;
-//   var server = new mongodb.Server('23.23.129.158', 27017, {});
-//   new mongodb.Db('militaryListDB', server, {}).open(function(err, client){
-//     if(err) throw err;
-
-//     var collection = new mongodb.Collection(client, 'products');
-//     collection.find({}).toArray(function(err, results){
-//       console.log(results);
-//       console.log(JSON.stringify(results));
-//       res.render('results', {result: JSON.stringify(results), title: 'Test'});
-//     });
-//   });
+// // app.js
+// db.Drinks.find({name: "20th Century"}, function(err, users) {
+//   if( err || !users) console.log("No female users found");
+//   else users.forEach( function(femaleUser) {
+//     console.log(femaleUser);
+//   } );
 // });
 
 
 
+//SAVE TO FILE
+// var fs = require('fs'); //import file system
+// fs.appendFile('fileStore/testFileIO.txt', 'Hey there Gregor!\n', function (err) {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log("The file was saved!");
+//     }
+// }); 
 
 var app = express(); //instantiates express
 
@@ -59,15 +61,131 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index); //sets start path to index in routes folder
-app.get('/recipes', routes.recipelist(db)); //when user navigates to to this route we pass the database object
-app.get('/recipes/:text', routes.recipelist(db));
+app.get('/recipes', recipes); //when user navigates to to this route we pass the database object
+app.get('/recipes/:key', recipes);
 // app.get('/myrecipes/:text'routes.myrecipes(request.params.text));
-app.get('/newrecipe', routes.newrecipe); //for adding a path to newrecipe
+//app.get('/newrecipe', routes.newrecipe); //for adding a path to newrecipe
 
-app.post('/addrecipe', routes.addrecipe(db)); //adding the the db
+//app.post('/addrecipe', routes.addrecipe(db)); //adding the the db
 
 //Creates server and loads express with the above environments set
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
+function recipes(req, res) {
+
+    var drinks;
+    var oneDrink;
+	if(req.params.key == null){
+		db.Drinks.find({}, function(err, Drinks){
+			if(err || !Drinks.length) console.log("User not found.");
+			else Drinks.forEach(function(designer){
+			console.log("User found! - " + designer.name + ' ' + designer.notes);
+			drinks = Drinks;
+			console.log("is there anybody home?");
+
+				db.Drinks.find({nameid: "20thCentury"}, function(err, drink){
+					if(err) console.log(err);
+					else { 
+						oneDrink = drink;
+						 console.log(oneDrink);    
+				        res.render('recipelist', { //pass on data
+				        "drinks_data" : drinks,
+				        // "foods_data" : foods,
+				        "drink_one": oneDrink
+				        // "food_one": oneFood,
+				        // "my_recipe": myrec
+				       	 
+				   	    });
+					}
+				});
+		
+			});
+		});
+
+		// db.Food.find({}, function(err, Food){
+		// 	if(err || !Food.length) console.log("User not found.");
+		// 	else Food.forEach(function(designer2){
+		// 	console.log("User found! - " + designer2.name + ' ' + designer2.notes);
+		// 	foods = Food;
+		// 	console.log("is there anybody home?");
+
+		// 		db.Food.find({nameid: "CraggyChocolateCake"}, function(err, one_food){
+		// 			if(err) console.log(err);
+		// 			else { 
+		// 				oneFood = one_food;
+		// 				 console.log(oneDrink);    
+		// 		        res.render('recipelist', { //pass on data
+		// 		        // "drinks_data" : drinks,
+		// 		        "foods_data" : foods,
+		// 		         // "drink_one": oneDrink,
+		// 		        "food_one": oneFood,
+		// 		        // "my_recipe": myrec
+				       	 
+		// 		   	    });
+		// 			}
+		// 		});
+		
+		// 	});
+		// });
+
+	} else{
+		db.Drinks.find({}, function(err, Drinks){
+			if(err || !Drinks.length) console.log("User not found.");
+			else Drinks.forEach(function(designer){
+			console.log("User found! - " + designer.name + ' ' + designer.notes);
+			drinks = Drinks;
+			console.log("is there anybody home?");
+
+				db.Drinks.find({nameid: req.params.key}, function(err, drink){
+					if(err) console.log(err);
+					else { 
+						oneDrink = drink;
+						 console.log("Smile");    
+				        res.render('recipelist', { //pass on data
+				        "drinks_data" : drinks,
+				        // "foods_data" : foods,
+				         "drink_one": oneDrink
+				        // "food_one": oneFood,
+				        // "my_recipe": myrec
+				       	 
+				   	    });
+					}
+				});
+		
+			});
+		});
+	}
+}	
+
+
+
+
+    //     var collection = db.get('recipecollection'); //sets the DB collection
+    //     var drinks;
+    //     var foods;
+    //     var choice  = '20th Century';
+    //     var key = req;
+    //     console.log(collection);
+    //     //var qs = new Querystring();
+    //     //var key = qs.get("key");
+    //     var test = choice;
+    //     // var byName = $.getUrlVar('name');
+    //     collection.find({"type":"cocktail"},{},function(e,drink){ //query DB
+    //         drinks = drink;
+    //     });
+    //     collection.find({"type":"food"},{},function(e,food){ //query DB
+    //         foods = food;
+    //     }); 
+    //     collection.find({"name": test},{},function(e,drinkItem){ //query DB
+    //         oneDrink = drinkItem;
+    //     });
+    //     collection.find({"name":"Fried PB&B"},{},function(e,foodItem){ //query DB
+    //         oneFood = foodItem;
+    //     }); 
+    //     // collection.find({mychoice},{},function(e,myrec){ //query DB
+    //     //     onerecipe = myrec;
+    //     // });                        
+    //     // console.log(drinks);
+    //     // console.log(foods); 
--- recipeSite/routes/index.js
+++ recipeSite/routes/index.js
@@ -32,43 +32,45 @@ exports.index = function(req, res){
 // }
 
 //passes data to reciplist.jade
-exports.recipelist = function(db) {
+// exports.recipelist = function(req, res) {
     
-        var collection = db.get('recipecollection'); //sets the DB collection
-        var drinks;
-        var foods;
-        var choice  = '20th Century';
-        //var qs = new Querystring();
-        //var key = qs.get("key");
-        var test = choice;
-        // var byName = $.getUrlVar('name');
-        collection.find({"type":"cocktail"},{},function(e,drink){ //query DB
-            drinks = drink;
-        });
-        collection.find({"type":"food"},{},function(e,food){ //query DB
-            foods = food;
-        }); 
-        collection.find({"name": test},{},function(e,drinkItem){ //query DB
-            oneDrink = drinkItem;
-        });
-        collection.find({"name":"Fried PB&B"},{},function(e,foodItem){ //query DB
-            oneFood = foodItem;
-        }); 
-        // collection.find({mychoice},{},function(e,myrec){ //query DB
-        //     onerecipe = myrec;
-        // });                        
-        console.log(drinks);
-        console.log(foods);   
-        return function(req, res) {     
-        res.render('recipelist', { //pass on data
-        "drinks_data" : drinks,
-        "foods_data" : foods,
-        "drink_one": oneDrink,
-        "food_one": oneFood,
-        // "my_recipe": myrec
-        });
-    };
-};
+//         var collection = db.get('recipecollection'); //sets the DB collection
+//         var drinks;
+//         var foods;
+//         var choice  = '20th Century';
+//         var key = req;
+//         console.log(key.params);
+//         //var qs = new Querystring();
+//         //var key = qs.get("key");
+//         var test = choice;
+//         // var byName = $.getUrlVar('name');
+//         collection.find({"type":"cocktail"},{},function(e,drink){ //query DB
+//             drinks = drink;
+//         });
+//         collection.find({"type":"food"},{},function(e,food){ //query DB
+//             foods = food;
+//         }); 
+//         collection.find({"name": test},{},function(e,drinkItem){ //query DB
+//             oneDrink = drinkItem;
+//         });
+//         collection.find({"name":"Fried PB&B"},{},function(e,foodItem){ //query DB
+//             oneFood = foodItem;
+//         }); 
+//         // collection.find({mychoice},{},function(e,myrec){ //query DB
+//         //     onerecipe = myrec;
+//         // });                        
+//         console.log(drinks);
+//         console.log(foods);   
+//         return function(req, res) {     
+//         res.render('recipelist', { //pass on data
+//         "drinks_data" : drinks,
+//         "foods_data" : foods,
+//         "drink_one": oneDrink,
+//         "food_one": oneFood,
+//         // "my_recipe": myrec
+//         });
+//     };
+// };
 
                 // each direction in drink.note      
                 //   div#recipeDiv
@@ -80,41 +82,41 @@ exports.newrecipe = function(req, res){
 };
 
 //For adding new data (posts)
-exports.addrecipe = function(db) {
-    return function(req, res) {
+// exports.addrecipe = function(db) {
+//     return function(req, res) {
 
-        // Get our form values. These rely on the "name" attributes
-        //<TODO> add dynamic values
-        //<TODO> add Prep time, cooking time, and servings to food
-        var recipeName = req.body.recipename;
-        var type = req.body.typeOfRecipe;
-        var ingred1 = req.body.ingredient1;
-        var ingred2 = req.body.ingredient2;
-        var ingred3 = req.body.ingredient3;
-        var ingred4 = req.body.ingredient4;
-        var notE = req.body.note;                                
+//         // Get our form values. These rely on the "name" attributes
+//         //<TODO> add dynamic values
+//         //<TODO> add Prep time, cooking time, and servings to food
+//         var recipeName = req.body.recipename;
+//         var type = req.body.typeOfRecipe;
+//         var ingred1 = req.body.ingredient1;
+//         var ingred2 = req.body.ingredient2;
+//         var ingred3 = req.body.ingredient3;
+//         var ingred4 = req.body.ingredient4;
+//         var notE = req.body.note;                                
 
-        // Set our collection
-        var collection = db.get('recipecollection');
+//         // Set our collection
+//         var collection = db.get('recipecollection');
 
-        // Submit to the DB
-        collection.insert({
-            "name" : recipeName,
-            "ingredient" : [ingred1, ingred2, ingred3, ingred4],
-            "note": notE,
-            "type" : type
-        }, function (err, doc) {
-            if (err) {
-                // If it failed, return error
-                res.send("There was a problem adding the information to the database.");
-            }
-            else {
-                // If it worked, forward to success page
-                res.redirect("recipes");
-                // And set the header so the address bar doesn't still say /addrecipe
-                res.location("recipes");
-            }
-        });
+//         // Submit to the DB
+//         collection.insert({
+//             "name" : recipeName,
+//             "ingredient" : [ingred1, ingred2, ingred3, ingred4],
+//             "note": notE,
+//             "type" : type
+//         }, function (err, doc) {
+//             if (err) {
+//                 // If it failed, return error
+//                 res.send("There was a problem adding the information to the database.");
+//             }
+//             else {
+//                 // If it worked, forward to success page
+//                 res.redirect("recipes");
+//                 // And set the header so the address bar doesn't still say /addrecipe
+//                 res.location("recipes");
+//             }
+//         });
 
-    }
-}
+//     }
+// }
